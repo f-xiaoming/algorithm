a1e5ef0efc615ea2615380f3ae797107518c4366
xiaofang <2492768843@qq.com>
2020-11-04 16:20:21 +0800

    目录更改

--- src/com/xiaofang/sort/FloatSort.java
+++ src/com/xiaofang/sort/FloatSort.java
@@ -7,29 +7,35 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-//生成随机数，并排序。文件输入输出。
-public class NumberSort {
+public class FloatSort {
     public static void main(String[] args) {
-        for (int i = 10000000; i <1000000000; i =i+50000000) {
+        for (int i = 100000; i < 10000000; i = i+500000) {
             System.out.print(i + "：");
             Test(i);
         }
     }
-    public static void Test(int n) {
+
+    public static void Test(int n){
         Random r = new Random(1);
         boolean flag = false;
-       // System.out.println("请输入要随机生成的数字的个数：");
+        //System.out.println("请输入要随机生成的浮点数的个数：");
+       // int n=0;
         //Scanner sc=new Scanner(System.in);
        // n=sc.nextInt();
-        int[] arr = new int[n];
+       // System.out.println("请输入浮点数的最大值：");
+        //float m=0;
+        //m=sc.nextFloat();
+        float[] arr = new float[n];
         for (int i = 0; i < n; i++) {
-            int ran1 = r.nextInt(n);
+            float ran1 = r.nextFloat();
+            ran1=ran1*n;
             arr[i] = ran1;
             //System.out.print(ran1+" ");
-        }
+        }//随机生成n个浮点数
 
         BufferedWriter bw1= null;
         OutputStreamWriter writer1 = null;
+
         BufferedWriter bw2= null;
         OutputStreamWriter writer2 = null;
         String str="";
@@ -37,26 +43,29 @@ public class NumberSort {
             File file1 = new File("E:\\Random.txt");
             writer1 = new OutputStreamWriter(new FileOutputStream(file1));
             bw1 = new BufferedWriter(writer1);
-           // for (int i = 0; i < n; i++) {
-                //str=String.valueOf(arr[i]);
-               // bw1.append(str);
-                //bw1.append(" ");
-            //}
+            for (int i = 0; i < n; i++) {
+                str=String.valueOf(arr[i]);
+                bw1.append(str);
+                bw1.append("   ");
+            }//把随机生成的随机浮点数放在E:\Random.txt中
+
             long startTime =System.currentTimeMillis();
             //numberSort(arr);
-            SortUtils.QuickSort(arr,0,n-1);
+            SortUtils.QuickSort1(arr,0,n-1);
             //Arrays.sort(arr);
             long endTime=System.currentTimeMillis();
             long time=endTime-startTime;
-            System.out.println("程序运行的时间为："+time+"ms");
+            System.out.println("程序运行的时间为："+time+"ms");//对随机生成的浮点数
+            // 进行排序并记录排序时间
+
             File file2 = new File("E:\\RandomSort.txt");
             writer2 = new OutputStreamWriter(new FileOutputStream(file2));
             bw2 = new BufferedWriter(writer2);
-            //for (int i = 0; i < n; i++) {
-               // str=String.valueOf(arr[i]);
-               // bw2.append(str);
-               // bw2.append(" ");
-           // }
+            for (int i = 0; i < n; i++) {
+                str=String.valueOf(arr[i]);
+                bw2.append(str);
+                bw2.append(" ");
+            }//把排好序的浮点数输入到E:\RandomSort.txt中
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -74,6 +83,7 @@ public class NumberSort {
 
         }
     }
+    //冒泡排序
     public static int[] numberSort(int []arr){
         boolean flag=false;
         for (int i = 0; i < arr.length - 1; i++) {
@@ -87,10 +97,12 @@ public class NumberSort {
                 }
             }
             if (flag == false) {
-                break;
+                        break;
             }
         }
         return arr;
     }
 
- }
+}
+
+
--- src/com/xiaofang/utils/SortUtils.java
+++ src/com/xiaofang/utils/SortUtils.java
@@ -146,6 +146,15 @@ public class SortUtils {
             QuickSort(array, smallIndex + 1, end);
         return array;
     }
+    public static float[] QuickSort1(float[] array, int start, int end) {
+        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
+        int smallIndex = partition1(array, start, end);
+        if (smallIndex > start)
+            QuickSort1(array, start, smallIndex - 1);
+        if (smallIndex < end)
+            QuickSort1(array, smallIndex + 1, end);
+        return array;
+    }
     /**
      * 6.1、快速排序算法——partition
      * @param array
@@ -165,6 +174,18 @@ public class SortUtils {
             }
         return smallIndex;
     }
+    public static int partition1(float[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap1(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap1(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
 
     /**
      * 6.2、交换数组内两个元素
@@ -174,6 +195,11 @@ public class SortUtils {
      */
     public static void swap(int[] array, int i, int j) {
         int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    public static void swap1(float[] array, int i, int j) {
+        float temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
